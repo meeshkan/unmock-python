d037ad5f8a359a53eeb8b5a0257a110286322532
Idan Tene <idan.tene@gmail.com>
2019-03-08 15:59:47 +0200

    Uses properties for easy testing

--- unmock/core/persistence.py
+++ unmock/core/persistence.py
@@ -42,66 +42,76 @@ class Persistence(ABC):
 
 class FSPersistence(Persistence):
     """File system based persistence layer"""
-    UNMOCK_DIR = Path.home().joinpath(".unmock")    # Get home directory for current user
-    TOKEN_PATH = UNMOCK_DIR.joinpath(".token")
-    CONFIG_PATH = UNMOCK_DIR.joinpath("credentials")
-    HASH_DIR = UNMOCK_DIR.joinpath("save")
+    HOMEPATH = Path.home()    # Get home directory for current user
     HEADERS_FILE = "response-header.json"
     BODY_FILE = "response.json"
 
     def __init__(self, token):
         super().__init__(token)
-        FSPersistence.UNMOCK_DIR.mkdir(parents=True, exist_ok=True)  # Create home directory if needed
+        self.unmock_dir.mkdir(parents=True, exist_ok=True)  # Create home directory if needed
 
-    @staticmethod
-    def __outdir(hash: str):
-        hashdir = FSPersistence.HASH_DIR.joinpath(hash)
+    @property
+    def unmock_dir(self):
+        return HOMEPATH.joinpath(".unmock")
+
+    @property
+    def token_path(self):
+        return self.unmock_dir.joinpath(".token")
+
+    @property
+    def config_path(self):
+        return self.unmock_dir.joinpath("credentials")
+
+    @property
+    def hash_dir(self):
+        return self.unmock_dir.joinpath("save")
+
+    def __outdir(self, hash: str):
+        hashdir = self.hash_dir.joinpath(hash)
         hashdir.mkdir(parents=True, exist_ok=True)
         return hashdir
 
-    @staticmethod
-    def __write_to_hashed(hash: str, filename: str, content: str):
-        with FSPersistence.__outdir(hash).joinpath(filename).open('w') as fp:
+    def __write_to_hashed(self, hash: str, filename: str, content: str):
+        with self.__outdir(hash).joinpath(filename).open('w') as fp:
             json.dump(content, fp, indent=2)
             fp.flush()
 
-    @staticmethod
-    def __load_from_hashed(hash: str, filename: str) -> Optional[Union[Dict[str, str], str]]:
+    def __load_from_hashed(self, hash: str, filename: str) -> Optional[Union[Dict[str, str], str]]:
         try:
-            with FSPersistence.__outdir(hash).joinpath(filename).open() as fp:
+            with self.__outdir(hash).joinpath(filename).open() as fp:
                 return json.load(fp)
         except (json.JSONDecodeError, OSError):  # Raise on other errors
             return None
 
     def save_body(self, hash: str, body: str = None, headers: str = None) -> None:
         if headers is not None:
-            FSPersistence.__write_to_hashed(hash=hash, filename=FSPersistence.HEADERS_FILE, content=headers)
+            self.__write_to_hashed(hash=hash, filename=FSPersistence.HEADERS_FILE, content=headers)
         if body is not None:
             json_body = "{}"
             try:
                 json_body = json.loads(body)
             except json.JSONDecodeError:
                 pass
-            FSPersistence.__write_to_hashed(hash=hash, filename=FSPersistence.BODY_FILE, content=json_body)
+            self.__write_to_hashed(hash=hash, filename=FSPersistence.BODY_FILE, content=json_body)
 
     def save_auth(self, auth: str) -> None:
-        with FSPersistence.TOKEN_PATH.open('w') as tknfd:
+        with self.token_path.open('w') as tknfd:
             tknfd.write(auth)
             tknfd.flush()
 
     def load_headers(self, hash: str) -> Optional[Dict[str, str]]:
-        return FSPersistence.__load_from_hashed(hash, FSPersistence.HEADERS_FILE)
+        return self.__load_from_hashed(hash, FSPersistence.HEADERS_FILE)
 
     def load_body(self, hash: str) -> Optional[Dict[str, str]]:
-        return FSPersistence.__load_from_hashed(hash, FSPersistence.BODY_FILE)
+        return self.__load_from_hashed(hash, FSPersistence.BODY_FILE)
 
     def load_auth(self) -> Optional[str]:
-        return FSPersistence.TOKEN_PATH.read_text()
+        return self.token_path.read_text()
 
     def load_token(self) -> Optional[str]:
         if self.token is not None:
             return self.token
-        if FSPersistence.CONFIG_PATH.exists():
+        if self.config_path.exists():
             iniparser = configparser.ConfigParser()
-            iniparser.read(FSPersistence.CONFIG_PATH)
+            iniparser.read(self.config_path)
             return iniparser.get("unmock", "token", fallback=None)
