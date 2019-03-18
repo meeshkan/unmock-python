2670b486b11c85ac2f39d9e9bd612a14f616d325
Idan Tene <idan.tene@gmail.com>
2019-03-18 15:42:55 +0200

    Offer 2 versions for the pytest plugin

--- unmock/pytest/plugin.py
+++ unmock/pytest/plugin.py
@@ -2,11 +2,23 @@
 import pytest
 from .. import init, reset, UnmockOptions
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def unmock():
+    """Initialized ONCE per module, saving time on pinging host etc"""
     def _init(**kwargs):
         reset()
         return init(**kwargs)
     init()
     yield _init
     reset()
+
+@pytest.fixture(scope="function")
+def unmock_local():
+    """Initializes the unmock service whenever used in any function"""
+    def _init(**kwargs):
+        reset()
+        return init(**kwargs)
+    init()
+    yield _init
+    reset()
+
